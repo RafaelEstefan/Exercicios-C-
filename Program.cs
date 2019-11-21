43c63a812ba8391ebd75944165c6bf632741833b
RafaelEstefan <52550811+RafaelEstefan@users.noreply.github.com>
2019-11-21 17:51:31 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -17,44 +17,7 @@ namespace Exercicios_C_
             ////Console.WriteLine(IMC(80, 1.75));
             //var entrada = Console.ReadLine();
             //Console.WriteLine(entrada);
-            //float A, P, imc;
-
-            //Console.Write("Digite o seu peso:");
-            //P = float.Parse(Console.ReadLine());
-
-
-            //Console.Write("Digite o seu altura:");
-            //A = float.Parse(Console.ReadLine());
-
-            //imc = (P / (A * A));
-
-            //if (imc <= 18)
-            //{
-            //    Console.WriteLine("Peso abaixo do normal");
-            //}
-            //else if ((imc > 18.5) && (imc < 24.9))
-            //{
-            //    Console.WriteLine("Peso normal");
-            //}
-
-            //else if ((imc >= 25) && (imc < 29.9))
-            //{
-            //    Console.WriteLine("Sobre o Peso");
-            //}
-            //else if ((imc >= 30) && (imc < 34.9))
-            //{
-            //    Console.WriteLine("Grau de Obesidade I");
-            //}
-            //else if ((imc >= 35) && (imc < 39.9))
-            //{
-            //    Console.WriteLine("Grau de Obesidade II");
-            //}
-            //else if (imc >= 40)
-            //{
-            //    Console.WriteLine("Obesidade Grau III");
-            //}
-
-            //Console.ReadKey();
+            
 
 
             //float num;
